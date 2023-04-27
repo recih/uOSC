e881df9fb7067199145f6c3c74462c7d46d21e09
recih <recih@users.noreply.github.com>
2023-04-27 14:14:13 +0800

    feat: add utils for read/create Message

---
+++ Runtime/Core/MessageBuilder.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace uOSC
+{
+    public class MessageBuilder
+    {
+        private string _address;
+        private readonly List<OCSValue> _buffer = new();
+
+        public MessageBuilder Address(string address)
+        {
+            _address = address;
+            return this;
+        }
+        
+        public MessageBuilder AddFloat(float value)
+        {
+            AddValue(value);
+            return this;
+        }
+        
+        public MessageBuilder AddInt(int value)
+        {
+            AddValue(value);
+            return this;
+        }
+        
+        public MessageBuilder AddString(string value)
+        {
+            AddValue(value);
+            return this;
+        }
+
+        public MessageBuilder AddVector2(Vector2 value)
+        {
+            AddValue(value.x);
+            AddValue(value.y);
+            return this;
+        }
+
+        public MessageBuilder AddVector3(Vector3 value)
+        {
+            AddValue(value.x);
+            AddValue(value.y);
+            AddValue(value.z);
+            return this;
+        }
+        
+        public MessageBuilder AddQuaternion(Quaternion value)
+        {
+            AddValue(value.x);
+            AddValue(value.y);
+            AddValue(value.z);
+            AddValue(value.w);
+            return this;
+        }
+
+        public void Reset()
+        {
+            _address = string.Empty;
+            _buffer.Clear();
+        }
+
+        public Message Build()
+        {
+            if (string.IsNullOrEmpty(_address))
+            {
+                throw new InvalidOperationException("Message address is null or empty, did you forget to call builder.Address()");
+            }
+            
+            var msg = new Message(_address, _buffer);
+            Reset();
+            
+            return msg;
+        }
+        
+        private MessageBuilder AddValue(OCSValue value)
+        {
+            _buffer.Add(value);
+            return this;
+        }
+    }
+}
